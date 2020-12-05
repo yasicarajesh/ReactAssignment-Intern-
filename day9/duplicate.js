b009a1b3aafaff97283d904a8b0787024148d23c
yasicarajesh <53334273+yasicarajesh@users.noreply.github.com>
2020-12-05 23:59:42 +0530

    Update duplicate.js

--- day9/duplicate.js
+++ day9/duplicate.js
@@ -1,5 +1,7 @@
- Array.prototype.beremoveobject =function(){
-   console.log(arr.filter((ele,indx)=>indx!==arr.indexOf(ele)))
+Array.prototype.beremoveobject =function(){ 
+   var dup=[];{
+   dup.push(arr.filter((ele,indx)=>indx!==arr.indexOf(ele))) }
+   return Array.from(new Set(dup));
 
 }
 var arr = [1,2,2,3,4,1];
