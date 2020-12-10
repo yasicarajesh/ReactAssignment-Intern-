2de59dd3c907a244c480600ef5cc51d835e02e63
yasicarajesh <53334273+yasicarajesh@users.noreply.github.com>
2020-12-10 17:13:43 +0530

    Add files via upload

--- reactpage/src/App.js
+++ reactpage/src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/text';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to BestEnlist React Workshop
-        </p>
-       
-      </header>
-    </div>
+   
+<Hello/>
+
+ 
   );
 }
 
